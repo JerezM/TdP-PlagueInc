0114929d588418143ada100f842c3dcea60cdf9c
JerezM <jerez149@gmail.com>
2020-12-04 10:43:19 -0300

    nivel controller modificado para ser una sola clase singleton

--- src/main/logica/nivel/NivelController.java
+++ src/main/logica/nivel/NivelController.java
@@ -1,9 +1,28 @@
 package main.logica.nivel;
 
-public interface NivelController {
-    
+public class NivelController {
+
+    private static NivelController instance;
+
+    private Nivel nivel;
+
+    private NivelController() {
+        nivel = Nivel.getInstance();
+    }
+
+    public static NivelController getInstance() {
+        if( instance == null ) {
+			instance = new NivelController();
+		}
+		
+		return instance;
+    }
+
     /**
-     * Se encarga de inicializar el mapa en su aspecto logico.
+     * Se encarga de inicializar el mapa y la oleada de enemigos en su aspecto logico para el nivel 1.
      */
-    public void generarMapa();
+    public void inicializarPartida() {
+        nivel.inicializarPartida();
+    }
+    
 }
