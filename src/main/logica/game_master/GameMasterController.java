6faae4caf260cea660e392ceaa3fe352be9933f3
JerezM <jerez149@gmail.com>
2020-12-04 10:44:23 -0300

    GameMasterController modificado para ser una sola clase singleton

--- src/main/logica/game_master/GameMasterController.java
+++ src/main/logica/game_master/GameMasterController.java
@@ -1,14 +1,37 @@
 package main.logica.game_master;
 
-public interface GameMasterController {
-    
-    /**
-     * Se encarga de inicializar el mapa y al jugador en su aspecto logico.
-     */
-    public void inicializarPartida();
+
+public class GameMasterController {
+
+    private static GameMasterController instance;
+
+    private GameMaster gameMaster;
+
+    private GameMasterController() {
+        gameMaster = GameMaster.getInstance();
+    }
+
+    public static GameMasterController getInstance() {
+        if( instance == null ) {
+			instance = new GameMasterController();
+		}
+		
+		return instance;
+    }
 
     /**
-     * Se encarga de generar la Oleada de enemigos.
+     * Se encarga de inicializar el mapa y el jugador en su aspecto logico.
      */
-    public void generarOleadaEnemigos();
+    public void inicializarPartida() {
+        gameMaster.inicializarPartida();
+    }
+
+    /**
+     * Se encarga de generar la oleada de enemigos para que empieze el nivel.
+     */
+    public void generarOleadaEnemigos() {
+        // TODO Auto-generated method stub
+
+    }
+    
 }
