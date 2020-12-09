b477cc8c21f590c7876bdfd73a1c9a5a655a5774
JerezM <jerez149@gmail.com>
2020-12-09 13:19:21 -0300

    GUI con mapa de fondo agregada

--- src/main/Juego.java
+++ src/main/Juego.java
@@ -1,12 +1,16 @@
 package main;
 
 import main.logica.game_master.GameMasterController;
+import main.view.GUI;
 
 public class Juego {
     public static void main(String[] args) {
 
         GameMasterController gameMasterController = GameMasterController.getInstance();
         gameMasterController.inicializarPartida();
+
+        GUI frame = GUI.getInstance();
+        frame.setVisible(true);
         
     }    
 }
--- src/main/view/GUI.java
+++ src/main/view/GUI.java
@@ -1,8 +1,43 @@
 package main.view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import main.view.mapa.MapaView;
 
 @SuppressWarnings("serial")
 public class GUI extends JFrame {
+
+    private static GUI instance;
+    private JPanel contentPane;
+
+    /**
+     * Create the frame.
+     */
+    private GUI() {
+        this.initComponents();
+	}
+	
+	public static GUI getInstance() {
+		if( instance == null ) {
+			instance = new GUI();
+		}
+		
+		return instance;
+    }
+    
+    private void initComponents() {
+
+        setTitle("PlagueInc");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(300, 25, 755, 637);
+        setResizable(false);
+
+		contentPane = MapaView.getInstance();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        
+    }
     
 }
