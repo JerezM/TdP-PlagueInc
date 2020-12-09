7aff504ad95315afd44d840cea0723b3b566c989
KatherinaVilca <katherinavilca@gmail.com>
2020-12-09 12:28:04 -0300

    Cambios en Oleada

--- src/infectado/InfectadoAlphaFactory.java
+++ src/infectado/InfectadoAlphaFactory.java
@@ -2,15 +2,8 @@ package infectado;
 
 public class InfectadoAlphaFactory implements InfectadoFactory{
 
-	private Infectado infectadoAlpha;
-	
-	private InfectadoAlphaFactory(int posicionCreacion) {
-	
-			infectadoAlpha= new InfectadoAlpha(posicionCreacion);
-	}
-	
 	public Infectado crearEnemigo() {
 		
-		return infectadoAlpha;
+		return new InfectadoAlpha(1);
 	}
 }
--- src/infectado/InfectadoBetaFactory.java
+++ src/infectado/InfectadoBetaFactory.java
@@ -2,16 +2,9 @@ package infectado;
 
 public class InfectadoBetaFactory implements InfectadoFactory{
 
-	private Infectado infectadoBeta;
-	
-	public InfectadoBetaFactory() {
-	
-		infectadoBeta= new InfectadoBeta(posicionCreacion);
-	}
-
 	@Override
 	public Infectado crearEnemigo() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new InfectadoBeta(1);
 	}
 }
