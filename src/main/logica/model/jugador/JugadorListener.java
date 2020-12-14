4805403ac2397afeb7f6c8bc2ee23f8c330f5b40
JerezM <jerez149@gmail.com>
2020-12-14 20:52:47 -0300

    interface jugadorListener agregada

---
+++ src/main/logica/model/jugador/JugadorListener.java
@@ -0,0 +1,10 @@
+package main.logica.model.jugador;
+
+public interface JugadorListener {
+    
+    /**
+     * Consulta el ultimo movimiento realizado por el jugador
+     * @return El ultimo movimiento que realizo el jugador.
+     */
+    public int getUltimoMovimiento();
+}
