2a1725b9b1824e4adef1cf05e5715b1a17b5e149
JerezM <jerez149@gmail.com>
2020-12-14 12:32:59 -0300

    metodo actualizarSprite en entidadGraficaJugador agregado

--- src/main/logica/entidad_grafica/EntidadGraficaJugador.java
+++ src/main/logica/entidad_grafica/EntidadGraficaJugador.java
@@ -32,9 +32,13 @@ public class EntidadGraficaJugador extends EntidadGrafica {
     /**
      * Actualiza el sprite actual por el correspondiente al movimiento parametrizado.
      * @param movimiento Indica el movimiento que se esta efectuando actualmente.
+     * 0=QUIETO; 1=MOV_IZQ; 2=MOV_DER; 3=DISPARANDO;
      * @return El nuevo sprite que representa el movimiento parametrizado.
      */
     public ImageIcon actualizarSprite(int movimiento) {
+        this.spriteIcon = new ImageIcon( this.getClass().getResource( sprites[movimiento] ) );
+        this.spriteIcon.setImage( this.spriteIcon.getImage() );
+        
         return spriteIcon;
     }
 
