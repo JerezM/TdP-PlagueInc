d010d813723053a4986957653adef11c1dca87f7
JerezM <jerez149@gmail.com>
2020-12-16 19:05:44 -0300

    Estructura basica de infectados agregada

---
+++ src/main/logica/model/infectado/Infectado.java
@@ -0,0 +1,8 @@
+package main.logica.model.infectado;
+
+public abstract class Infectado {
+    
+    protected Infectado() {
+        
+    }
+}
---
+++ src/main/logica/model/infectado/InfectadoAlpha.java
@@ -0,0 +1,10 @@
+package main.logica.model.infectado;
+
+import java.awt.Point;
+
+public class InfectadoAlpha extends Infectado {
+    
+    public InfectadoAlpha(Point posicionCreacion) {
+
+    }
+}
