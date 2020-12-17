9e303b62c68075c0acc11c907db0881c460c0b86
JerezM <jerez149@gmail.com>
2020-12-17 18:25:04 -0300

    clase EntidadModel agregada, junto con metodos relacionados en la clase GameMaster

--- src/main/logica/game_master/GameMaster.java
+++ src/main/logica/game_master/GameMaster.java
@@ -3,6 +3,7 @@ package main.logica.game_master;
 import java.util.LinkedList;
 import java.util.List;
 
+import main.logica.model.EntidadModel;
 import main.logica.model.jugador.JugadorController;
 import main.logica.model.jugador.JugadorListener;
 import main.logica.model.jugador.JugadorListenerImpl;
@@ -16,7 +17,7 @@ public class GameMaster extends Thread {
     private JugadorController jugadorController;
     private JugadorListener jugadorListener;
 
-    // private List<EntidadModel> entidades;
+    private List<EntidadModel> entidades;
 
     private GameMaster() {
 
@@ -24,7 +25,7 @@ public class GameMaster extends Thread {
         jugadorController = JugadorController.getInstance();
         jugadorListener = JugadorListenerImpl.getInstance();
 
-        // entidades = new LinkedList<EntidadModel>();
+        entidades = new LinkedList<EntidadModel>();
     }
 
     public static GameMaster getInstance() {
@@ -53,22 +54,20 @@ public class GameMaster extends Thread {
     @Override
     public void run() {
 
-        boolean jugadorVivo = true;
-        // List<EntidadModel> entidadesActuales;
+        boolean jugadorVivo = jugadorController.isAlive();
+        List<EntidadModel> entidadesActuales;
         int frames = 0;
-        while (jugadorVivo && nivelController.hasNextLevel()) {// Mientras queden niveles por jugar y el jugador este
-                                                               // vivo
+
+        while (jugadorVivo && nivelController.hasNextLevel()) {// Mientras queden niveles por jugar y el jugador este vivo
 
             nivelController.startNextLevel();
 
-            while (jugadorVivo /* && !nivelController.isLevelComplete() */ ) {// Mientras no haya completado el nivel y
-                                                                              // el jugador este vivo
+            while (jugadorVivo && !nivelController.isLevelComplete()) {// Mientras no haya completado el nivel y el jugador este vivo
 
-                /*
-                 * entidadesActuales = new LinkedList<EntidadModel>(entidades);
-                 * 
-                 * for(EntidadModel entidad : entidadesActuales) { entidad.update(); }
-                 */
+                entidadesActuales = new LinkedList<EntidadModel>(entidades);
+                 
+                for(EntidadModel entidad : entidadesActuales) { entidad.update(); }
+                 
 
                 int movimientoJugador = this.getUltimoMovimientoJugador();
                 System.out.println("frame: " + frames++);
@@ -82,19 +81,35 @@ public class GameMaster extends Thread {
 
                 //masterCollider.checkCollisions();
 
-                //nivelController.update();
+                nivelController.update();
 
-                //jugadorVivo = jugadorController.isAlive();
+                jugadorVivo = jugadorController.isAlive();
             }
         }
 
         if(jugadorVivo) {
-            //YOU WIN
+            System.out.println("YOU WIN!!");
         }
         else {
-            //YOU LOSE
+            System.out.println("YOU LOSE!!");
         }
 
+    }
+
+    /**
+     * Se encarga de agregar la entidad parametrizada a la lista de entidades activas.
+     * @param entidad Entidad a agregar.
+     */
+    public synchronized void addEntidad(EntidadModel entidad) {
+        entidades.add(entidad);
+    }
+
+    /**
+     * Se encarga de remover la entidad parametrizada de la lista de entidades activas.
+     * @param entidad Entidad a remover.
+     */
+    public synchronized void removeEntidad(EntidadModel entidad) {
+        entidades.remove(entidad);
     }
 
     /**
